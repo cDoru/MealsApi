6571288b8809361236d5fd1d77620385a61d33b1
Doru Cioclea <cioclea.doru@gmail.com>
2017-03-29 11:20:15 +0300

    Add CORS support

---
+++ MealsApi/MealsApi/Utils/CustomCorsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http.Cors;
+
+namespace MealsApi.Utils
+{
+    public class CustomCorsAttribute : Attribute, ICorsPolicyProvider
+    {
+        private readonly CorsPolicy _policy;
+
+        public CustomCorsAttribute()
+        {
+            _policy = new CorsPolicy
+            {
+                AllowAnyHeader = true,
+                AllowAnyMethod = true
+            };
+
+            _policy.Origins.Add("localhost");
+        }
+
+        public Task<CorsPolicy> GetCorsPolicyAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_policy);
+        }
+    }
+}
