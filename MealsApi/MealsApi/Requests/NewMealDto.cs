21ff5e9cb3f2a2cfb8c23276ebb48f46229b046c
Doru Cioclea <cioclea.doru@gmail.com>
2017-03-29 14:50:54 +0300

    Add meal request dto

---
+++ MealsApi/MealsApi/Requests/NewMealDto.cs
@@ -0,0 +1,13 @@
+﻿using MealsApi.Utils.Upload;
+
+namespace MealsApi.Requests
+{
+    public class NewMealDto
+    {
+        public string Name { get; set; }
+
+        public HttpFile Image { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
